7c95211459ab30617520a57ec4c9dd75e1f6f499
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 18:04:29 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+/*Implemente una función que reciba dos strings como parámetro y devuelva 1 si son
+iguales y 0 en caso contrario. No se puede usar la función strcmp() de la librería
+        string.h.*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
